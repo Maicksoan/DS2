3bf152d74b5a69b7347f76c40caa5abec3809e27
Maicksoan <maickvarelam@gmail.com>
2019-08-27 20:22:16 -0300

    Update cidade.controller.js

--- mvc-join/controller/cidade.controller.js
+++ mvc-join/controller/cidade.controller.js
@@ -63,15 +63,15 @@ module.exports = {
     },
     update: (req, res) => {
         //Atualizar o id do objeto do req.body
-        req.body.id = req.params.id;
+        const cidade = {
+            nome: req.body.nome,
+            estado_id: req.body.estado.id,
+            id: req.params.id
+        }
 
-        repository.update(req.body, (error, result) => {
+        repository.update(cidade, (error, result) => {
             if (error) {
                 res.status(500).send(error);
-            }
-
-            if (result.affectedRows == 0) {
-                res.status(404).send('not found');
             }
 
             res.send(result);
